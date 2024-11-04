6517c60a5371eb05096e724f34ddabdc91f567c3
Una L <una.lestrange@atu.ie>
2024-11-04 09:45:49 +0000

    task1done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { GradeHistoriesComponent } from './grade-histories/grade-histories.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', redirectTo: '/'},
+    {path: 'grade-history', component: GradeHistoriesComponent},
     ];
