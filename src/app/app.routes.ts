a8070750f7bc75387751a896746fd5d2bd893457
Una L <una.lestrange@atu.ie>
2024-11-15 14:45:19 +0000

    populate edit form properly is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { GradeHistoriesComponent } from './grade-histories/grade-histories.component';
 import { HomeComponent } from './home/home.component';
 import { TestFormComponent } from './test-form/test-form.component';
+import { GradeHistoryDetailsComponentComponent } from './grade-history-details-component/grade-history-details-component.component';
 
 
 
@@ -9,5 +10,7 @@ export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', redirectTo: '/'},
     {path: 'grade-history', component: GradeHistoriesComponent},
+    {path: 'grade-history/:id', component: GradeHistoryDetailsComponent},
     {path: 'form', component: TestFormComponent},
     ];
+f
